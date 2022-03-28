b7fdfab369a1324ddbb5f57b6120b79b1b6d3c18
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-28 08:49:58 +0600

    explored button in react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>using react bootstrap</h1>
+      <Button variant='danger'>My button</Button>
     </div>
   );
 }
