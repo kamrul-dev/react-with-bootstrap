251ad3e0b01cdc7174071fb649244471877adc63
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-28 08:34:38 +0600

    Use Bootstrap CDN in React in regular way

--- src/components/CardGroup/CardGroup.js
+++ src/components/CardGroup/CardGroup.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Card from '../Card/Card';
 
 const CardGroup = () => {
+    const products = [
+        {id: 1, name: 'Laptop dell', price: 70000},
+        {id: 2, name: 'Laptop hp', price: 70000},
+        {id: 3, name: 'Laptop lenevo', price: 70000}
+    ]
     return (
         <div>
             <h2>This is my Cards</h2>
+            <div className="card-group">
+                {
+                    products.map(product => <Card
+                        key={product.id}
+                        product={product}
+                    ></Card>)
+                }
+            </div>
         </div>
     );
 };
