08a307c35bd2986abbb6f7a258f73ded6c18e959
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-28 08:54:04 +0600

    explored spinner in react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Button } from 'react-bootstrap';
+import { Button, Spinner } from 'react-bootstrap';
 import './App.css';
 
 
@@ -6,7 +6,10 @@ function App() {
   return (
     <div className="App">
       <h1>using react bootstrap</h1>
-      <Button variant='danger'>My button</Button>
+      <Button variant='danger'>My button</Button> <br /> <br />
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
     </div>
   );
 }
